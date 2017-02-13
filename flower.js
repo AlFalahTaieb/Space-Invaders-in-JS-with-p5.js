9bf423470d35d04185e584df2c42e0e026f4372c
TaiebAlFalah <mohamedtaieb.alfalah@esprit.tn>
2017-02-13 17:07:26 +0100

    add flower

--- flower.js
+++ flower.js
@@ -1,16 +1,27 @@
-/**
- * Created by Taieb on 13/02/2017.
- */
-function Flower() {
-    this.x = width / 2;
-    this.y=50;
+function Flower(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 30;
 
-    this.show = function () {
-        fill(255,0,200);
+    this.xdir = 1;
 
-        ellipse(this.x, this.y,60,60);
+    this.grow = function() {
+        this.r = this.r + 2;
+    }
+
+    this.shiftDown = function() {
+        this.xdir *= -1;
+        this.y += this.r;
+    }
+
+    this.move = function() {
+        this.x = this.x + this.xdir;
+    }
+
+    this.show = function() {
+        noStroke();
+        fill(255, 0, 200, 150);
+        ellipse(this.x, this.y, this.r*2, this.r*2);
     }
 
 }
-
-
